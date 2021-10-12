c71cce45c3f94a683d450dbe1141dd75d83bb3e0
Jungbin Jung <43312096+dev-madilyn@users.noreply.github.com>
2021-10-12 18:04:00 +0900

    [CHORE] Add activityIndicator

--- RxSwiftBasic/RxSwiftBasic/ViewController.swift
+++ RxSwiftBasic/RxSwiftBasic/ViewController.swift
@@ -8,7 +8,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    lazy var activityIndicator: UIActivityIndicatorView = {
+            // Create an indicator.
+            let activityIndicator = UIActivityIndicatorView()
+            activityIndicator.frame = CGRect(x: 0, y: 0, width: 50, height: 50)
+            activityIndicator.center = self.view.center
+            activityIndicator.color = UIColor.red
+            // Also show the indicator even when the animation is stopped.
+            activityIndicator.hidesWhenStopped = true
+            activityIndicator.style = UIActivityIndicatorView.Style.white
+            // Start animation.
+            activityIndicator.stopAnimating()
+            return activityIndicator
+            
+        }()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.

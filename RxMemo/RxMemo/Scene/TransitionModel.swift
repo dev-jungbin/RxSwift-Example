c1a0e7b33e7262cd69f891c6ffda14920f909209
Jungbin Jung <lollin0@naver.com>
2021-12-26 23:50:48 +0900

    Create: 전환 방식, 에러 enum 구현

---
+++ RxMemo/RxMemo/Scene/TransitionModel.swift
@@ -0,0 +1,23 @@
+//
+//  TransitionModel.swift
+//  RxMemo
+//
+//  Created by 1v1 on 2021/12/26.
+//
+
+import Foundation
+
+// 전환 방식 열거형
+enum TransitionStyle {
+    case root
+    case push
+    case modal
+}
+
+// 전환 시 에러 형식 선언
+
+enum TransitionError: Error {
+    case navigationControllerMissing
+    case cannotPop
+    case unknown
+}

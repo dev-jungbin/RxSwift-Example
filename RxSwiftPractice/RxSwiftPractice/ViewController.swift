0b90d750932bc0651f189bbed0409d9b6fa326a6
Jungbin Jung <lollin0@naver.com>
2022-01-06 16:07:41 +0900

    Chore: Practice Rx TextField

--- RxSwiftPractice/RxSwiftPractice/ViewController.swift
+++ RxSwiftPractice/RxSwiftPractice/ViewController.swift
@@ -7,10 +7,11 @@
 
 import UIKit
 import RxSwift
+import RxCocoa
 
 class SignUpViewController: UIViewController {
-    @IBOutlet weak var IDTextFIeld: UITextField!
-    @IBOutlet weak var PWTextField: UITextField!
+    @IBOutlet weak var PWFirstTextField: UITextField!
+    @IBOutlet weak var PWSecondTextField: UITextField!
     @IBOutlet weak var infoLabel: UILabel!
     @IBOutlet weak var enterBtn: UIButton!
     
@@ -18,12 +19,24 @@ class SignUpViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        PWSecondTextField.rx.text
+            .orEmpty // optional 걸러 줌
+            .skip(1) // 첫 번째 이벤트는 무시
+            .distinctUntilChanged() // editingEvents 이벤트는 무시, 값 바뀔 때만 ㅇㅇ
+            .subscribe(onNext: { changedText in
+                if changedText != self.PWFirstTextField.text {
+                    DispatchQueue.main.async {
+                        self.infoLabel.isHidden = false
+                    }
+                } else {
+                    DispatchQueue.main.async {
+                        self.infoLabel.isHidden = true
+                    }
+                }
+            })
+            .disposed(by: disposeBag)
+        
     }
-    
-    func initIDTextField() {
-        self.IDTextFIeld.rx.control
-    }
-
 
 }
 
